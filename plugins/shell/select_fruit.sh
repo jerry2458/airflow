730d6b418bd4a14d3a5452c2eedcd6207d9c4b2c
jerry <jerry2458@naver.com>
2024-03-29 21:53:12 +0900

    plugins 폴더 추가

---
+++ plugins/shell/select_fruit.sh
@@ -0,0 +1,10 @@
+FRUIT=$1
+if [ $FRUIT == APPLE ] ; then
+	echo "APPLE"
+elif [ $FRUIT == ORANGE ] ; then
+	        echo "ORANGE"
+elif [ $FRUIT == GRAPE ] ; then
+	        echo "GRAPE"
+else
+	echo "Other"
+fi
